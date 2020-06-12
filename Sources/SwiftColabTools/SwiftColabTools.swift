6df5439fb7b916e224906079513afc1195250ec2
bwilfley <drclearly@gmail.com>
2020-06-11 18:13:36 -0700

    Add normMarsaglia

--- Sources/SwiftColabTools/SwiftColabTools.swift
+++ Sources/SwiftColabTools/SwiftColabTools.swift
@@ -1,3 +1,16 @@
 struct SwiftColabTools {
     var text = "Hello, World!"
 }
+
+public func normMarsaglia( ) -> Double {
+  var S = 1.1
+  var U = Double()
+  var V = Double()
+  while( S >= 1.0) {
+    U = Double.random(in: -1..<1)
+    V = Double.random(in: -1..<1)
+    S = U*U + V*V
+    //print(S)
+  }
+  return U * sqrt((-2.0 * log(S))/S)
+}
