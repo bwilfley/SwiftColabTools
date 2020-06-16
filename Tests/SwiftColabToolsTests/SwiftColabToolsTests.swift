d689c3ae812161e754966c91bd601897ab51ea18
bwilfley <drclearly@gmail.com>
2020-06-16 07:54:54 -0700

    Add normMarsaglia

--- Tests/SwiftColabToolsTests/SwiftColabToolsTests.swift
+++ Tests/SwiftColabToolsTests/SwiftColabToolsTests.swift
@@ -6,7 +6,7 @@ final class SwiftColabToolsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftColabTools().text, "Hello, World!")
+        XCTAssertEqual(Tools().text, "Hello, World!")
     }
 
     static var allTests = [
